97d6efcb97b061ab646f7f6f77c2af86060722f0
robin <rogo009git@gmail.com>
2020-02-11 21:11:39 -0800

    update script notes

--- script.js
+++ script.js
@@ -62,15 +62,6 @@ if(auto) {
     slideInterval = setInterval(nextSlide, intervalTime);
 }
 
-// ReadME
-// target the slides
-// target the buttons
-// create a variables for auto scroll and interval time
-// essentially this entire project is to create function(s) that add or remove a class name called 'current' to make images appear or disappear if button is clicked and / or when auto scroll is activated "true"
-// you have to apply a class that tracks the slides
-// resetting the interval prevented the slide /img (if the user clicked before 5 seconds passed) from being shown less than 5 seconds. each click ensures each slide will always have a 5 second interval re-applied upon each button click
-// this project has images as background, so the imgs were implemented into the css file
-
 // const nextSlide = function() {
 
 // }
